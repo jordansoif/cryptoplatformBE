4e26c1cb7c7fe4494fb9fcaa3cc331092a0d9be5
Jordan <48663692+jordansoif@users.noreply.github.com>
2019-09-16 22:18:35 -0400

    9/16 end of night commit

--- app.py
+++ app.py
@@ -14,9 +14,7 @@ from keys import *
 from user.model import Users, Holdings, Realized_Positions, Purchase_Lots
 from auth import api as auth_api, login_page_parser
 from user import api as user_api
-# from classSchemas import *
-# from parsers import *
-# from app import *
+from bson.objectid import ObjectId
 
 # Init App
 app = Flask(__name__)
@@ -46,7 +44,6 @@ def update_bitcoin_parser():
     parser.add_argument("bitcoin")
     return parser
 
-
 def trade_page_purchase_parser():
     parser = reqparse.RequestParser()
     parser.add_argument("user", required=True)
@@ -56,7 +53,6 @@ def trade_page_purchase_parser():
     parser.add_argument("units_purchased")
     return parser
 
-
 def trade_page_sale_parser():
     parser = reqparse.RequestParser()
     parser.add_argument("user", required=True)
@@ -65,41 +61,63 @@ def trade_page_sale_parser():
     parser.add_argument("share_price", type=float)
     parser.add_argument("trade_value_calc", type=float)
     parser.add_argument("total_shares_being_sold", type=float)
-    parser.add_argument("sale_lots", action="append")
+    parser.add_argument("sale_lots", type=dict, action="append")
+    return parser
+
+def get_user_symbol_parser():
+    parser = reqparse.RequestParser()
+    parser.add_argument("user", required=True)
+    parser.add_argument("symbol")
     return parser
 
 
 # HOLDINGS PAGE ROUTES
 
 # Get All Holdings
-@api.route("/getallholdings/<user>")
+@api.route("/getallholdings")
 class get_all_holdings(Resource):
-    def get(self, user):
+    def put(self):
+        parser = get_user_symbol_parser()
+        args = parser.parse_args()
+        user = args["user"]
         user_finder = Users.objects(user_name=user).first()
         holdings_finder = user_finder.holdings
         return [holding.serializer_holdings() for holding in holdings_finder]
-        # BELOW WILL HELP A BUNCH, FOUND IN MONGO DOCS
-        # Prints out the names of all the users in the database
-        # for user in User.objects:
-        #     print user.name
 
 # Get All Symbols in Holdings
-@api.route("/getallsymbolholdings/<user>")
+@api.route("/getallsymbolholdings")
 class get_all_symbol_holdings(Resource):
-    def get(self, user):
+    def put(self):
+        parser = get_user_symbol_parser()
+        args = parser.parse_args()
+        user = args["user"]
         user_finder = Users.objects(user_name=user).first()
         holdings_finder = user_finder.holdings
-        return [holding.serializer_symbol_only() for holding in holdings_finder]
+        return [holding.serializer_holdings() for holding in holdings_finder]
+
+
+# Get All Realized from User
+@api.route("/getallrealized")
+class get_all_realized(Resource):
+    def put(self):
+        parser = get_user_symbol_parser()
+        args = parser.parse_args()
+        user = args["user"]
+        user_finder = Users.objects(user_name=user).first()
+        realized_finder = user_finder.realized_positions
+        return [realized.serializer_realized_gain_loss_display() for realized in realized_finder]
 
 # FUND ACCOUNT PAGE ROUTES
 
 # Get User bitcoin
-@api.route("/getuserbitcoin")
+@api.route("/getuserbitcoin/<user>")
 class get_user(Resource):
-    @jwt_required
-    def get(self):
-        current_user = get_jwt_identity() #this is returning an id
-        user_finder = Users.objects(id=current_user).first()
+    # @jwt_required
+    def get(self,user):
+        # current_user = get_jwt_identity() #this is returning an id
+        # user_finder = Users.objects(id=current_user).first()
+        # return user_finder.bitcoin
+        user_finder = Users.objects(user_name=user).first()
         return user_finder.bitcoin
 
 # Update User bitcoin
@@ -136,13 +154,8 @@ class purchase_crypto(Resource):
         symbol_finder = user_finder.holdings.filter(symbol=new_symbol).first()
         current_bitcoin = user_finder.bitcoin
         if symbol_finder == None:
-            add_new_lot_no_holding = Purchase_Lots()
-            add_new_lot_no_holding.units_purchased = new_units_purchased
-            add_new_lot_no_holding.cost_per_unit = new_purchase_price
-            new_holding = Holdings()
-            new_holding.symbol = new_symbol
-            new_holding.units_held = new_units_purchased
-            new_holding.average_cost_per_unit = new_purchase_price
+            add_new_lot_no_holding = Purchase_Lots(units_purchased = new_units_purchased, cost_per_unit = new_purchase_price)
+            new_holding = Holdings(symbol = new_symbol,units_held = new_units_purchased,average_cost_per_unit = new_purchase_price,)
             new_holding.purchase_lots.append(add_new_lot_no_holding)
             user_finder.holdings.append(new_holding)
             user_finder.update(set__bitcoin=(
@@ -157,12 +170,7 @@ class purchase_crypto(Resource):
                 (new_units_purchased/new_units_held)*new_purchase_price)
             symbol_finder.units_held = new_units_held
             symbol_finder.average_cost_per_unit = new_average_cost_per_unit
-            # exchange_time = client.get_server_time()
-            # exchange_time_value = exchange_time["serverTime"]
-            # FIX ABOVE LATER IF YOU WANT, DEFAULT SHOULD WORK
-            add_new_lot = Purchase_Lots()
-            add_new_lot.units_purchased = new_units_purchased
-            add_new_lot.cost_per_unit = new_purchase_price
+            add_new_lot = Purchase_Lots(units_purchased = new_units_purchased,cost_per_unit = new_purchase_price)
             symbol_finder.purchase_lots.append(add_new_lot)
             user_finder.update(set__bitcoin=(
                 current_bitcoin-(new_purchase_price*new_units_purchased)))
@@ -178,81 +186,61 @@ class sell_crypto(Resource):
     def put(self):
         parser = trade_page_sale_parser()
         args = parser.parse_args()
-        arg_user = args['user'] #Making payload info usable
+        arg_user = args['user'] #Making payload info usable #THIS NEEDS TO CHANGE TO USER ID
         arg_symbol = args['symbol']
         arg_share_price = args['share_price']
         arg_trade_value_calc = args['trade_value_calc']
         arg_total_shares_being_sold = args['total_shares_being_sold']
-        arg_sale_lots = args['sale_lots']
-        return arg_sale_lots
-        user_finder = Users.objects(user_name=arg_user).first() #Find user
+        arg_sale_lots = args['sale_lots'] # this now works, returns a list(array) of dicts(objects)
+        user_finder = Users.objects(user_name=arg_user).first() #Find user # THIS WILL CHANGE TO USER ID
         holdings_finder = user_finder.holdings.filter(symbol=arg_symbol) #Find symbol being traded
+        # Purchase_lot adjustment
         purchase_lots_finder = holdings_finder[0]['purchase_lots']  #Find tax lots for the holding
-        # purchase_to_sale_lot_filter = [purchase_lots_finder.filter(purchase_date_time = lots.purchase_date_time).update(units_purchased= (self.units_purchased - lots.units_purchased)) for lots in arg_sale_lots]  # Loop arg_sale_lots to find the matching purchase lots
-        # TEST BELOW
-        purchase_to_sale_lot_filter = [purchase_lots_finder.filter(units_purchased = lots.units_purchased) for lots in arg_sale_lots]  # Loop arg_sale_lots to find the matching purchase lots
-        # TEST ABOVE   +++ .update(units_purchased= (self.units_purchased - lots.units_purchased)
-        # purchase_lot_sale_update= purchase_to_sale_lot_filter.filter(units_purchased=0).delete() # Delete function for now empty purchase lots
-        # purchase_lot_sale_update.save()
-        
-        # def purchase_lot_sale_update(self, sale_lot_filter):
-        #     if self.purchase_date_time == sale_lot_filter.purchase_date_time:
-        #         update_units_purchased = self.units_purchased - sale_lot_filter.units_purchased
-
-        
-
-        # purchase_lot_sale_update.filter(units_purchased=0).delete() # Delete function for now empty purchase lots
-
-        #Purchase Lots: Loop over arg_sale_lots.saleLotInfo to filter purchase lots for potential matches of purchase dates
-            #If found, subtract arg_sale_lots.salesLot.value from matched purchase lot
-                #if units_purchase now == 0, delete the lot
-        #Holdings: subtract arg_total_shares_being_sold from holdings.units_held
-            #make a formula to adjust the avg_cost_per_unit
-                #if purchase_lots.length = 0, delete the holding from user
-        #Realized Positions: create and add a new realized position for each sale lot
-            #build calculations for the remaining fields in realized_pos that arent in args
-        #Users: add the arg_trade_calc_value to bitcoin balance
-            #remove holding.symbol if no more units are held
-                #add realized positions as mentioned above
-
-
-
-        # symbol_finder = user_finder.holdings.objects(
-        #     symbol=new_symbol).first()
-        # lot_finder = symbol_finder.objects(cost_per_unit=new_unit_cost).first()
-        # lot_purchase_date_time = lot_finder.purchase_date_time
-        # lot_cost_per_unit = lot_finder.cost_per_unit
-        # if request.method == "PUT":
-        #     add_realized_position = Realized_Positions(
-        #         new_symbol, lot_purchase_date_time, sale_date_time, lot_cost_per_unit, new_unit_cost, new_units_sold)
-        #     user_finder.realized_positions.append(add_realized_position)
-        #     user_finder.save()
-        # if new_units_sold >= lot_finder.units_purchase:
-        #     symbol_finder.objects(cost_per_unit=new_unit_cost).first().delete()
-        #     symbol_finder.save()
-        # else:
-        #     return "Security was not sold."
-
-
-# Get All Symbols in Holdings
-@api.route("/getallsymbolholdings/<user>")
-class get_all_symbol_holdings(Resource):
-    def get(self, user):
-        user_finder = Users.objects(user_name=user).first()
-        holdings_finder = user_finder.holdings
-        return [holding.serializer_symbol_only() for holding in holdings_finder]
+        for lots in arg_sale_lots: # loop over sale_lots to find matching purchase lots and update them
+            lot_finder = purchase_lots_finder.filter(_id = ObjectId(lots["saleLotInfo"]["id"]))
+            if lot_finder:
+                found_lot_units_purchased = lot_finder[0]['units_purchased']
+                lot_finder.update(units_purchased= found_lot_units_purchased - lots["value"])
+        purchase_lots_finder.filter(units_purchased=0).delete() # Delete function for now empty purchase lots
+        # Holdings adjustment
+        holdings_finder.update(units_held=(holdings_finder[0]["units_held"]-arg_total_shares_being_sold))
+        if purchase_lots_finder.count() == 0: # delete a holding if it no longer has purchase_lots, this means you longer hold any units in the holding
+            holdings_finder.delete()
+                    #make a formula to adjust the avg_cost_per_unit
+        #Realized Positions adjustment
+        for lots in arg_sale_lots:
+            new_realized_position = Realized_Positions(
+                    symbol = arg_symbol,
+                    purchase_date_time = lots["saleLotInfo"]["purchase_date_time"],
+                    sale_date_time = datetime.datetime.now(),
+                    purchase_price = lots["saleLotInfo"]["cost_per_unit"],
+                    sale_price = arg_share_price,
+                    units_sold = lots["value"],
+                    profit_loss = (arg_share_price - lots["saleLotInfo"]["cost_per_unit"])*lots["value"],
+                    profit_loss_percent = (arg_share_price/lots["saleLotInfo"]["cost_per_unit"])/100
+            )
+            user_finder.realized_positions.append(new_realized_position)
+        # Users adjustment
+        user_finder.update(bitcoin= user_finder.bitcoin + arg_trade_value_calc)
+        # END TEST
+        user_finder.save()
+        return "Action completed"
 
 
 # Get User Purchase Lots for Single Symbol
-@api.route("/getsymbolpurchaselots/<user>/<symbol>")
+@api.route("/getsymbolpurchaselots")
 class get_all_symbol_purchase_lots(Resource):
-    def get(self, user, symbol):
+    def put(self):
+        parser = get_user_symbol_parser()
+        args = parser.parse_args()
+        user = args["user"]
+        symbol = args["symbol"]
         user_finder = Users.objects(user_name=user).first()
         holdings_finder = user_finder.holdings.filter(symbol=symbol)
         serialized_holding = [purchase_lot.serializer_test()
                               for purchase_lot in holdings_finder]
         purchase_lot_finder = serialized_holding[0]["purchase_lots"]
-        return [lot.serializer_purchase_lots() for lot in purchase_lot_finder]
+        return purchase_lot_finder
 
 
 # BINANCE API ROUTES
@@ -277,13 +265,12 @@ class two_day_kline(Resource):
         return klines
 
 
-@api.route("/getallsymbols")
+@api.route("/getallsymbols") #Get all available tickers from binance
 class get_all_symbols(Resource):
     def get(self):
         all_symbols = client.get_all_tickers()
         return all_symbols
 
-# Schema Setup
 
 
 if __name__ == '__main__':
--- user/model.py
+++ user/model.py
@@ -1,14 +1,21 @@
 from mongoengine import *
 import datetime
+from bson.objectid import ObjectId
+from binance.client import Client
+from keys import *
+
+client = Client(API_KEY, API_SECRET)
 
 
 class Purchase_Lots(EmbeddedDocument):
+    _id = ObjectIdField(default=lambda: ObjectId(), required=True)
     purchase_date_time = DateTimeField(default=datetime.datetime.now())
     units_purchased = FloatField(default=0.00)
     cost_per_unit = FloatField(default=0.00)
 
     def serializer_purchase_lots(self):
         return{
+            "id": str(self._id),
             "purchase_date_time": self.purchase_date_time.isoformat(),
             "units_purchased": self.units_purchased,
             "cost_per_unit": self.cost_per_unit
@@ -55,6 +62,18 @@ class Realized_Positions(EmbeddedDocument):
     units_sold = FloatField(default=0.00)
     profit_loss = FloatField(default=0.00)
     profit_loss_percent = FloatField(default=0.00)
+
+    def serializer_realized_gain_loss_display(self):
+        return{
+            "symbol": self.symbol,
+            "purchase_date_time": self.purchase_date_time.isoformat(),
+            "sale_date_time": self.sale_date_time.isoformat(),
+            "purchase_price": self.purchase_price,
+            "sale_price": self.sale_price,
+            "units_sold": self.units_sold,
+            "profit_loss": self.profit_loss,
+            "profit_loss_percent": self.profit_loss_percent
+        }
 
 
 class Users(Document):
